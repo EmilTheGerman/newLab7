933cf61c654761f500d800c84d81410da6a4ed84
EmilTheGerman <c.nimets.emilien@student.uzhnu.edu.ua>
2026-06-27 18:48:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Task1();
+
+        Console.WriteLine("\nНатиснiть будь-яку клавiшу для завершення...");
+        Console.ReadKey();
+
+
+    }
+
+    static void Task1()
+    {
+        Console.WriteLine("========== Завдання 1 ==========\n");
+
+        Console.WriteLine("Неправильний варiант:");
+
+        List<Action> actions = new List<Action>();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            actions.Add(() => Console.WriteLine(i));
+        }
+
+        foreach (Action action in actions)
+            action();
+
+        Console.WriteLine("\nПравильний варiант:");
+
+        actions.Clear();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            int copy = i;
+
+            actions.Add(() => Console.WriteLine(copy));
+        }
+
+        foreach (Action action in actions)
+            action();
+    }
+}
