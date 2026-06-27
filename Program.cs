f5e6306c76af0fcc1929a1c8be04e1a62c4ee4a9
EmilTheGerman <c.nimets.emilien@student.uzhnu.edu.ua>
2026-06-27 18:55:04 +0300

    f

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ class Program
     {
         Task1();
 
+        Console.WriteLine("\nНатиснiть будь-яку клавiшу...");
+        Console.ReadKey();
+        Console.Clear();
+
+        Task2();
+
         Console.WriteLine("\nНатиснiть будь-яку клавiшу для завершення...");
         Console.ReadKey();
 
@@ -42,5 +48,37 @@ class Program
 
         foreach (Action action in actions)
             action();
+    }
+    static void Task2()
+    {
+        Console.WriteLine("========== Завдання 2 ==========\n");
+
+        BankTerminal terminal = new BankTerminal();
+
+        terminal.OnMoneyWithdraw += amount =>
+        {
+            Console.WriteLine($"Повiдомлення: з рахунку знято {amount} грн.");
+        };
+
+        terminal.Withdraw(500);
+
+        Console.WriteLine();
+        Console.WriteLine("Спроба виконати:");
+        Console.WriteLine("terminal.OnMoneyWithdraw = null;");
+        Console.WriteLine("terminal.OnMoneyWithdraw?.Invoke(1000);");
+
+        Console.WriteLine();
+        Console.WriteLine("Обидвi операцiї викликають помилки компiляцiї,");
+        Console.WriteLine("оскiльки використано ключове слово event.");
+    }
+}
+class BankTerminal
+{
+    public event Action<int>? OnMoneyWithdraw;
+
+    public void Withdraw(int amount)
+    {
+        Console.WriteLine($"Знято {amount} грн.");
+        OnMoneyWithdraw?.Invoke(amount);
     }
 }
